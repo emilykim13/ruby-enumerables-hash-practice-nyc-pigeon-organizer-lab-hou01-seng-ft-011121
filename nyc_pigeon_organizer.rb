253c28c1455c01a36fe47fc59ea9d6409ce4be14
Emily Nguyen <emilyn0614@gmail.com>
2021-01-04 12:55:28 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,7 +5,9 @@ def nyc_pigeon_organizer(data)
     attributes.each do |data_1, names|
       names.each do |name|
         pigeon_list[name] ||= {}
+          # must be a hash 
         pigeon_list[name][trait] ||= []
+          # ^ must be an array
         pigeon_list[name][trait] << data_1.to_s 
       end 
     end
