98f93f59ceaea61891cd0dcf9df6d8c5e488b89a
Emily Nguyen <emilyn0614@gmail.com>
2021-01-04 12:52:58 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,10 +2,10 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   pigeon_list = {}
   data.each do |trait, attributes|
-    attributes.each do |designs, names|
+    attributes.each do |data1, names|
       names.each do |name|
         pigeon_list[name] || = {}
-        pigeon_list[name][trait] << details.to_s 
+        pigeon_list[name][trait] << data1.to_s 
       end 
     end
   end
